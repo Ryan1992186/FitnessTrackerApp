548e31771a1127f129e3be977301f0ad1c0a6e62
Ryan Hillier <ryan1992186@gmail.com>
2021-08-16 23:51:07 -0500

    setup server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const logger = require('morgan');
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fitnesstracker', {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true, 
+  useCreateIndex: true 
+});
+
+require('./routes/api')(app);
+require('./routes/html')(app);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
